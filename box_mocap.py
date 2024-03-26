cf17488dae2eac08583c2ed83a23b9ad2d4ca813
Max Nagy <maxmunzel@me.com>
2024-03-26 16:17:23 +0100

    mocap: add cli

--- box_mocap.py
+++ box_mocap.py
@@ -1,14 +1,21 @@
 from relative import main
 import redis
 import json
+import typer
 
-redis_connection = redis.Redis()
 
-for frame in main():
-    marker_id, transform = frame
-    marker_id = int(marker_id)
-    x, y = transform[:2, 3].flatten()
-    transform = list(transform.flatten())
-    payload = {"transform": json.dumps(transform)}
-    redis_connection.xadd("box_tracking", payload)
-    print(f"x={x:.2f}\ty={y:.2f}")
+def mocap(redis_ip: str = "127.0.0.1"):
+    redis_connection = redis.Redis(redis_ip)
+
+    for frame in main():
+        marker_id, transform = frame
+        marker_id = int(marker_id)
+        x, y = transform[:2, 3].flatten()
+        transform = list(transform.flatten())
+        payload = {"transform": json.dumps(transform)}
+        redis_connection.xadd("box_tracking", payload)
+        print(f"x={x:.2f}\ty={y:.2f}")
+
+
+if __name__ == "__main__":
+    typer.run(mocap)
