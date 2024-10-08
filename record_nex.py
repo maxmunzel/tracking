ecd0b1cece4f252ff639ce3ef76ba9a5266aff67
Max Nagy <maxmunzel@me.com>
2024-10-08 17:21:43 +0200

    Add record_nex.py

---
+++ record_nex.py
@@ -0,0 +1,35 @@
+import cv2
+import typer
+
+
+def main(file: str, preview: bool = True, cam_id: int = 1):
+    cam = cv2.VideoCapture(cam_id)
+    out = cv2.VideoWriter(file, cv2.VideoWriter_fourcc(*"mp4v"), 3, (1620, 1080))
+    assert out.isOpened()
+
+    try:
+        i = 0
+        while True:
+            i += 1
+            ret, frame = cam.read()
+            frame = frame[:, :1620, :]  # crop away menu
+            assert ret
+            if not i % 10:
+                out.write(frame)
+
+            # frame = frame[:, ::-1, :]
+
+            if preview:
+                # Display the frame
+                cv2.imshow("Frame", frame)
+
+                # Break the loop
+                if cv2.waitKey(1) & 0xFF == ord("q"):
+                    break
+    finally:
+        out.release()
+        cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    typer.run(main)
